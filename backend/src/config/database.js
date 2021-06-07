4a9db1f272fdf0fa3112df047071b944b7bc9584
Gabriel Moraes <gabriel.moraes@ceresinvestimentos.com>
2021-06-07 19:16:17 -0300

    feat: add connection to database

---
+++ backend/src/config/database.js
@@ -0,0 +1,11 @@
+module.exports = {
+  dialect: 'postgres',
+  host: 'localhost',
+  username: 'postgres',
+  password: 'postgres',
+  database: 'recette',
+  define: {
+    timestamps: true,
+    underscored: true
+  }
+}
