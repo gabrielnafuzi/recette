6877f9ef958e769f0b46fe163e5f7d4137b8fbf5
Gabriel Moraes <gabrielnafuzi@gmail.com>
2021-06-20 04:01:49 -0300

    feat: add bullet component

---
+++ frontend/src/components/atoms/Bullet.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="w-5 h-5 bg-orange--base flex items-center justify-center rounded-full">
+    <span v-if="index" class="text-sm font-bold text-light-200">{{ index }}</span>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  props: {
+    index: {
+      type: String,
+      required: false,
+      default: '',
+    },
+  },
+})
+</script>
+
+<style scoped></style>
