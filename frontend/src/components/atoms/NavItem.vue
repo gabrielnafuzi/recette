d1d29ac2923ef20272daca9cbdfa0be318597d16
Gabriel Moraes <gabrielnafuzi@gmail.com>
2021-06-20 03:37:21 -0300

    feat: add nav item component

---
+++ frontend/src/components/atoms/NavItem.vue
@@ -0,0 +1,32 @@
+<template>
+  <router-link :to="to" class="nav-item" active-class="active">
+    {{ text }}
+  </router-link>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  props: {
+    text: {
+      type: String,
+      required: true,
+    },
+    to: {
+      type: String,
+      required: true,
+    },
+  },
+})
+</script>
+
+<style scoped>
+.nav-item {
+  @apply font-semibold text-lg text-typo--darken transition-colors hover:text-orange--base;
+}
+
+.nav-item.active {
+  @apply text-orange--base;
+}
+</style>
