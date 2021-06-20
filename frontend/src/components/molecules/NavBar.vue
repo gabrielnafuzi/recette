2ba5a7a0a3e36d653582e71606cb75ac057eef0b
Gabriel Moraes <gabrielnafuzi@gmail.com>
2021-06-20 13:33:57 -0300

    feat: add navbar component

---
+++ frontend/src/components/molecules/NavBar.vue
@@ -0,0 +1,24 @@
+<template>
+  <nav>
+    <NavItem text="Receitas" to="/" class="mr-6" />
+    <NavItem
+      v-if="auth.currentUser"
+      text="Minhas receitas"
+      to="/my-recipes"
+      class="mr-6"
+    />
+    <NavItem
+      v-if="auth.currentUser?.role === 'admin'"
+      text="Admin"
+      to="/admin"
+      class="mr-6"
+    />
+    <NavItem text="Sair" to="/logout" class="mr-6" />
+  </nav>
+</template>
+
+<script lang="ts" setup>
+import { useAuthStore } from '@/store'
+
+const auth = useAuthStore()
+</script>
