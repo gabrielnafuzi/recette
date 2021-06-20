1b2a5e27d4bc5f0abccef9e4af56cc7c8085bc89
Gabriel Moraes <gabrielnafuzi@gmail.com>
2021-06-20 03:45:56 -0300

    feat: add link component

---
+++ frontend/src/components/atoms/Link.vue
@@ -0,0 +1,28 @@
+<template>
+  <router-link
+    :to="to"
+    class="
+      text-lg text-orange--base font-bold underline underline-offset-auto
+      transition-colors hover:text-orange--darken
+    "
+  >
+    {{ text }}
+  </router-link>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  props: {
+    text: {
+      type: String,
+      required: true,
+    },
+    to: {
+      type: String,
+      required: true,
+    },
+  },
+})
+</script>
