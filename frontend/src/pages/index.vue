86528a81479c641079040eca4d6ce6ff5d0adee3
Gabriel Moraes <gabriel.moraes@ceresinvestimentos.com>
2021-06-22 01:09:51 -0300

    chore: update index

--- frontend/src/pages/index.vue
+++ frontend/src/pages/index.vue
@@ -1,8 +1,16 @@
 <template>
-  <h1>
-    Home
-  </h1>
+  <div
+    class="grid md:grid-cols-2 py-8 gap-12 gap-y-14 lg:grid-cols-3 sm:grid-cols-1 xl:grid-cols-3"
+  />
 </template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  setup() {},
+})
+</script>
 
 <route lang="yaml">
 meta:
