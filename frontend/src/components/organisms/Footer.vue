f581ddc6fd7a5dff182ce0536678c60a3249d5c3
Gabriel Moraes <gabriel.moraes@ceresinvestimentos.com>
2021-06-24 01:38:26 -0300

    chore: add padding to footer

--- frontend/src/components/organisms/Footer.vue
+++ frontend/src/components/organisms/Footer.vue
@@ -1,5 +1,5 @@
 <template>
-  <footer class="w-full text-center mt-auto">
+  <footer class="w-full text-center mt-auto pb-4">
     <p class="text-typo-lighten">
       &copy; {{ new Date().getFullYear() }} Recette - Todos os diretos reservados
     </p>
