6a4ab385bfc1b5e78af500b0ab57084568e0d39f
Gabriel Moraes <gabriel.moraes@ceresinvestimentos.com>
2021-06-24 01:45:19 -0300

    chore: update header logo to go to home on click

--- frontend/src/components/organisms/Header.vue
+++ frontend/src/components/organisms/Header.vue
@@ -1,6 +1,12 @@
 <template>
   <header class="flex flex-col sm:(items-center flex-row) justify-between">
-    <Logo />
+    <Logo class="cursor-pointer" @click="router.push('/')" />
     <NavBar class="<sm:mt-3" />
   </header>
 </template>
+
+<script lang="ts" setup>
+import { useRouter } from 'vue-router'
+
+const router = useRouter()
+</script>
