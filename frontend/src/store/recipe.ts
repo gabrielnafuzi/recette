1196befcb64928dbd60813684ff0fec316f2df96
Gabriel Moraes <gabriel.moraes@ceresinvestimentos.com>
2021-06-25 23:09:33 -0300

    feat: add recipe store

---
+++ frontend/src/store/recipe.ts
@@ -0,0 +1,11 @@
+import { defineStore } from 'pinia'
+import { api } from '@/services'
+
+const useRecipeStore = defineStore({
+  id: 'recipe',
+  state: () => ({}),
+  getters: {},
+  actions: {},
+})
+
+export default useRecipeStore
