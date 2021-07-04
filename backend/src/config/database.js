5bda421e50bbd3061b093bb070e926e927f5fe6f
Gabriel Moraes <gabriel.moraes@ceresinvestimentos.com>
2021-07-04 04:44:06 -0300

    chore: add ssl true

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -4,6 +4,13 @@ module.exports = {
   username: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_DATABASE,
+  port: 5432,
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false
+    }
+  },
   define: {
     timestamps: true,
     underscored: true
