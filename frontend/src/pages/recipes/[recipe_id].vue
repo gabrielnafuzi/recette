53cd77c078f977f3d49a3487ef62616f7fa08638
Gabriel Moraes <gabriel.moraes@ceresinvestimentos.com>
2021-07-04 01:42:45 -0300

    feat: add redirect to home when user is not allowed to see a recipe

--- frontend/src/pages/recipes/[recipe_id].vue
+++ frontend/src/pages/recipes/[recipe_id].vue
@@ -5,14 +5,23 @@
 </template>
 
 <script lang="ts" setup>
-import { useRoute } from 'vue-router'
+import { useRoute, useRouter } from 'vue-router'
 import { useRecipeStore } from '@/store'
+import { NOT_ALLOWED_TO_SEE_ERROR_MESSAGE } from '@/constants'
 
 const route = useRoute()
-
+const router = useRouter()
 const recipeStore = useRecipeStore()
 
-const getRecipe = async () => await recipeStore.show(route.params.recipe_id as string)
+const getRecipe = async () => {
+  try {
+    await recipeStore.show(route.params.recipe_id as string)
+  } catch (e) {
+    if (e.message === NOT_ALLOWED_TO_SEE_ERROR_MESSAGE) {
+      router.push('/')
+    }
+  }
+}
 
 if (!Object.keys(recipeStore.recipe).length) {
   getRecipe()
